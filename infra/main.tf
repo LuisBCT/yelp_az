409daebe63ea3e9555719f1f62b8a407a8506e83
luislinux <luisct1123@gmail.com>
2026-03-04 20:44:30 -0500

    modified terraform files to add a storage account

--- infra/main.tf
+++ infra/main.tf
@@ -2,3 +2,13 @@ resource "azurerm_resource_group" "rg" {
   name     = "rsgyelpaz"
   location = "East US"
 }
+
+# Storage Account que depende de rsgyelpaz
+resource "azurerm_storage_account" "stgyelpaz" {
+  name                     = var.storage_account_name         
+  resource_group_name      = azurerm_resource_group.rg.name 
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+}
